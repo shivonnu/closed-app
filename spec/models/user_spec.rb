09360191b1726b18b1c6adb6b06c168ba3e4bf32
shivonnu <shivonnu@gmail.com>
2020-10-07 22:33:26 +0900

    単体モデルコード追記
    単体モデルコード追記
    サインイン、サインアウト確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,6 +23,12 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Email is invalid")
     end
+    it "重複したemailが存在する場合登録できないこと" do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
     it "passwordが空では登録できない" do
       @user.password = ""
       @user.valid?

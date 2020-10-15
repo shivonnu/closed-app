2a15785d4ea4d0dbff1c39e8c84a284148518895
shivonnu <shivonnu@gmail.com>
2020-10-16 08:07:15 +0900

    チェック終了時にメッセージを出すように修正
    チェック終了時にメッセージを出すようにjavascriptを修正

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,68 +1,69 @@
 addEventListener("load",function() {
+let ele1_1 = document.getElementById("buttun1ok");
+let ele1_2 = document.getElementById("buttun1ng");
+let ele2_1 = document.getElementById("buttun2ok");
+let ele2_2 = document.getElementById("buttun2ng");
+let ele3_1 = document.getElementById("buttun3ok");
+let ele3_2 = document.getElementById("buttun3ng");
+let ele4_1 = document.getElementById("buttun4ok");
+let ele4_2 = document.getElementById("buttun4ng");
 
-let ele1_1 = document.getElementById('buttun1ok');
-let ele1_2 = document.getElementById('buttun1ng');
-let ele2_1 = document.getElementById('buttun2ok');
-let ele2_2 = document.getElementById('buttun2ng');
-let ele3_1 = document.getElementById('buttun3ok');
-let ele3_2 = document.getElementById('buttun3ng');
-let ele4_1 = document.getElementById('buttun4ok');
-let ele4_2 = document.getElementById('buttun4ng');
-
-const ok1_1 = ele1_1.style.display;
-const ng1_1 = ele1_2.style.display;
-const ok2_1 = ele2_1.style.display;
-const ng2_1 = ele2_2.style.display;
-const ok3_1 = ele3_1.style.display;
-const ng3_1 = ele3_2.style.display;
-const ok4_1 = ele4_1.style.display;
-const ng4_1 = ele4_2.style.display;
-
-ele1_1.style.display = 'none';
-ele2_1.style.display = 'none';
-ele3_1.style.display = 'none';
-ele4_1.style.display = 'none';
+ele1_2.innerText = "×";
+ele2_2.innerText = "×";
+ele3_2.innerText = "×";
+ele4_2.innerText = "×";
 
 ele1_1.onclick = function(){
-  ele1_1.style.display = 'none';
-  ele1_2.style.display = ng1_1;
-}
+  ele1_1.innerText = "";
+  ele1_2.innerText = "×";
+};
 ele1_2.onclick = function(){
-  ele1_2.style.display = 'none';
-  ele1_1.style.display = ok1_1;
-}
+  ele1_2.innerText = "";
+  ele1_1.innerText = "○";
+};
 
 ele2_1.onclick = function(){
-  ele2_1.style.display = 'none';
-  ele2_2.style.display = ng2_1;
-}
+  ele2_1.innerText = "";
+  ele2_2.innerText = "×";
+};
 
 ele2_2.onclick = function(){
-  ele2_2.style.display = 'none';
-  ele2_1.style.display = ok2_1;
-}
+  ele2_2.innerText = "";
+  ele2_1.innerText = "○";
+};
 
 ele3_1.onclick = function(){
-  ele3_1.style.display = 'none';
-  ele3_2.style.display = ng3_1;
-}
+  ele3_1.innerText = "";
+  ele3_2.innerText = "×";
+};
 
 ele3_2.onclick = function(){
-  ele3_2.style.display = 'none';
-  ele3_1.style.display = ok3_1;
-}
+  ele3_2.innerText = "";
+  ele3_1.innerText = "○";
+};
 
 ele4_1.onclick = function(){
-  ele4_1.style.display = 'none';
-  ele4_2.style.display = ng4_1;
-}
+  ele4_1.innerText = "";
+  ele4_2.innerText = "×";
+};
 
 ele4_2.onclick = function(){
-  ele4_2.style.display = 'none';
-  ele4_1.style.display = ok4_1;
-}
+  ele4_2.innerText = "";
+  ele4_1.innerText = "○";
+};
+});
 
+addEventListener("click",function() {
+  let ele1_1 = document.getElementById("buttun1ok").innerText;
+  let ele2_1 = document.getElementById("buttun2ok").innerText;
+  let ele3_1 = document.getElementById("buttun3ok").innerText;
+  let ele4_1 = document.getElementById("buttun4ok").innerText;
+  let check_ok = document.getElementById("check_ok");
+  
+  check_ok.innerText ="";
 
-})
+if (ele1_1 != "" && ele1_1 == ele2_1 && ele3_1 == ele4_1 && ele2_1 == ele3_1 )check_ok.innerText = "お疲れ様でした！";
+});
+
 
 

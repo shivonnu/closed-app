137a266f7b5d43fe1320ed50844f9a638c0b9e69
shivonnu <shivonnu@gmail.com>
2020-10-21 20:19:36 +0900

    人数が変更出来るように設定
    人数が変更出来るように設定しました。

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -19,125 +19,490 @@ let ele9_1 = document.getElementById("buttun9ok");
 let ele9_2 = document.getElementById("buttun9ng");
 let ele10_1 = document.getElementById("buttun10ok");
 let ele10_2 = document.getElementById("buttun10ng");
-
-ele1_2.innerText = "×";
-ele2_2.innerText = "×";
-ele3_2.innerText = "×";
-ele4_2.innerText = "×";
-ele5_2.innerText = "×";
-ele6_2.innerText = "×";
-ele7_2.innerText = "×";
-ele8_2.innerText = "×";
-ele9_2.innerText = "×";
-ele10_2.innerText = "×";
-
-ele1_1.onclick = function(){
-  ele1_1.innerText = "";
-  ele1_2.innerText = "×";
-};
-ele1_2.onclick = function(){
-  ele1_2.innerText = "";
-  ele1_1.innerText = "○";
-};
-
-ele2_1.onclick = function(){
-  ele2_1.innerText = "";
-  ele2_2.innerText = "×";
-};
-
-ele2_2.onclick = function(){
-  ele2_2.innerText = "";
-  ele2_1.innerText = "○";
-};
-
-ele3_1.onclick = function(){
-  ele3_1.innerText = "";
-  ele3_2.innerText = "×";
-};
-
-ele3_2.onclick = function(){
-  ele3_2.innerText = "";
-  ele3_1.innerText = "○";
-};
-
-ele4_1.onclick = function(){
-  ele4_1.innerText = "";
-  ele4_2.innerText = "×";
-};
-
-ele4_2.onclick = function(){
-  ele4_2.innerText = "";
-  ele4_1.innerText = "○";
-};
-
-ele5_1.onclick = function(){
-  ele5_1.innerText = "";
-  ele5_2.innerText = "×";
-};
-
-ele5_2.onclick = function(){
-  ele5_2.innerText = "";
-  ele5_1.innerText = "○";
-};
-
-ele6_1.onclick = function(){
-  ele6_1.innerText = "";
-  ele6_2.innerText = "×";
-};
-
-ele6_2.onclick = function(){
-  ele6_2.innerText = "";
-  ele6_1.innerText = "○";
-};
-
-ele7_1.onclick = function(){
-  ele7_1.innerText = "";
-  ele7_2.innerText = "×";
-};
-
-ele7_2.onclick = function(){
-  ele7_2.innerText = "";
-  ele7_1.innerText = "○";
-};
-ele8_1.onclick = function(){
-  ele8_1.innerText = "";
-  ele8_2.innerText = "×";
-};
-
-ele8_2.onclick = function(){
-  ele8_2.innerText = "";
-  ele8_1.innerText = "○";
-};
-ele9_1.onclick = function(){
-  ele9_1.innerText = "";
-  ele9_2.innerText = "×";
-};
-
-ele9_2.onclick = function(){
-  ele9_2.innerText = "";
-  ele9_1.innerText = "○";
-};
-ele10_1.onclick = function(){
-  ele10_1.innerText = "";
-  ele10_2.innerText = "×";
-};
-
-ele10_2.onclick = function(){
-  ele10_2.innerText = "";
-  ele10_1.innerText = "○";
-};
+let people_1 = document.getElementById("people_1_button")
+let people_2 = document.getElementById("people_2_button")
+let people_3 = document.getElementById("people_3_button")
+let people_4 = document.getElementById("people_4_button")
+let people_5 = document.getElementById("people_5_button")
+let people_6 = document.getElementById("people_6_button")
+let people_7 = document.getElementById("people_7_button")
+let people_8 = document.getElementById("people_8_button")
+let people_9 = document.getElementById("people_9_button")
+let people_10 = document.getElementById("people_10_button")
+let people_1_maru = document.getElementById("peoplemaru_1")
+let people_2_maru = document.getElementById("peoplemaru_2")
+let people_3_maru = document.getElementById("peoplemaru_3")
+let people_4_maru = document.getElementById("peoplemaru_4")
+let people_5_maru = document.getElementById("peoplemaru_5")
+let people_6_maru = document.getElementById("peoplemaru_6")
+let people_7_maru = document.getElementById("peoplemaru_7")
+let people_8_maru = document.getElementById("peoplemaru_8")
+let people_9_maru = document.getElementById("peoplemaru_9")
+let people_10_maru = document.getElementById("peoplemaru_10")
+  people_1.onclick = function(){
+    people_1_maru.innerText = "○";
+    people_2_maru.innerText = "";
+    people_3_maru.innerText = "";
+    people_4_maru.innerText = "";
+    people_5_maru.innerText = "";
+    people_6_maru.innerText = "";
+    people_7_maru.innerText = "";
+    people_8_maru.innerText = "";
+    people_9_maru.innerText = "";
+    people_10_maru.innerText = "";
+    ele1_2.innerText = "×";
+    ele2_2.innerText = "";
+    ele3_2.innerText = "";
+    ele4_2.innerText = "";
+    ele5_2.innerText = "";
+    ele6_2.innerText = "";
+    ele7_2.innerText = "";
+    ele8_2.innerText = "";
+    ele9_2.innerText = "";
+    ele10_2.innerText = "";
+    ele1_1.innerText = "";
+    ele2_1.innerText = "";
+    ele3_1.innerText = "";
+    ele4_1.innerText = "";
+    ele5_1.innerText = "";
+    ele6_1.innerText = "";
+    ele7_1.innerText = "";
+    ele8_1.innerText = "";
+    ele9_1.innerText = "";
+    ele10_1.innerText = "";
+  };
+  people_2.onclick = function(){
+    people_1_maru.innerText = "";
+    people_2_maru.innerText = "○";
+    people_3_maru.innerText = "";
+    people_4_maru.innerText = "";
+    people_5_maru.innerText = "";
+    people_6_maru.innerText = "";
+    people_7_maru.innerText = "";
+    people_8_maru.innerText = "";
+    people_9_maru.innerText = "";
+    people_10_maru.innerText = "";
+    ele1_2.innerText = "×";
+    ele2_2.innerText = "×";
+    ele3_2.innerText = "";
+    ele4_2.innerText = "";
+    ele5_2.innerText = "";
+    ele6_2.innerText = "";
+    ele7_2.innerText = "";
+    ele8_2.innerText = "";
+    ele9_2.innerText = "";
+    ele10_2.innerText = "";
+    ele1_1.innerText = "";
+    ele2_1.innerText = "";
+    ele3_1.innerText = "";
+    ele4_1.innerText = "";
+    ele5_1.innerText = "";
+    ele6_1.innerText = "";
+    ele7_1.innerText = "";
+    ele8_1.innerText = "";
+    ele9_1.innerText = "";
+    ele10_1.innerText = "";
+  };
+  people_3.onclick = function(){
+    people_1_maru.innerText = "";
+    people_2_maru.innerText = "";
+    people_3_maru.innerText = "○";
+    people_4_maru.innerText = "";
+    people_5_maru.innerText = "";
+    people_6_maru.innerText = "";
+    people_7_maru.innerText = "";
+    people_8_maru.innerText = "";
+    people_9_maru.innerText = "";
+    people_10_maru.innerText = "";
+    ele1_2.innerText = "×";
+    ele2_2.innerText = "×";
+    ele3_2.innerText = "×";
+    ele4_2.innerText = "";
+    ele5_2.innerText = "";
+    ele6_2.innerText = "";
+    ele7_2.innerText = "";
+    ele8_2.innerText = "";
+    ele9_2.innerText = "";
+    ele10_2.innerText = "";
+    ele1_1.innerText = "";
+    ele2_1.innerText = "";
+    ele3_1.innerText = "";
+    ele4_1.innerText = "";
+    ele5_1.innerText = "";
+    ele6_1.innerText = "";
+    ele7_1.innerText = "";
+    ele8_1.innerText = "";
+    ele9_1.innerText = "";
+    ele10_1.innerText = "";
+  };
+  people_4.onclick = function(){
+    people_1_maru.innerText = "";
+    people_2_maru.innerText = "";
+    people_3_maru.innerText = "";
+    people_4_maru.innerText = "○";
+    people_5_maru.innerText = "";
+    people_6_maru.innerText = "";
+    people_7_maru.innerText = "";
+    people_8_maru.innerText = "";
+    people_9_maru.innerText = "";
+    people_10_maru.innerText = "";
+    ele1_2.innerText = "×";
+    ele2_2.innerText = "×";
+    ele3_2.innerText = "×";
+    ele4_2.innerText = "×";
+    ele5_2.innerText = "";
+    ele6_2.innerText = "";
+    ele7_2.innerText = "";
+    ele8_2.innerText = "";
+    ele9_2.innerText = "";
+    ele10_2.innerText = "";
+    ele1_1.innerText = "";
+    ele2_1.innerText = "";
+    ele3_1.innerText = "";
+    ele4_1.innerText = "";
+    ele5_1.innerText = "";
+    ele6_1.innerText = "";
+    ele7_1.innerText = "";
+    ele8_1.innerText = "";
+    ele9_1.innerText = "";
+    ele10_1.innerText = "";
+  };
+  people_5.onclick = function(){
+    people_1_maru.innerText = "";
+    people_2_maru.innerText = "";
+    people_3_maru.innerText = "";
+    people_4_maru.innerText = "";
+    people_5_maru.innerText = "○";
+    people_6_maru.innerText = "";
+    people_7_maru.innerText = "";
+    people_8_maru.innerText = "";
+    people_9_maru.innerText = "";
+    people_10_maru.innerText = "";
+    ele1_2.innerText = "×";
+    ele2_2.innerText = "×";
+    ele3_2.innerText = "×";
+    ele4_2.innerText = "×";
+    ele5_2.innerText = "×";
+    ele6_2.innerText = "";
+    ele7_2.innerText = "";
+    ele8_2.innerText = "";
+    ele9_2.innerText = "";
+    ele10_2.innerText = "";
+    ele1_1.innerText = "";
+    ele2_1.innerText = "";
+    ele3_1.innerText = "";
+    ele4_1.innerText = "";
+    ele5_1.innerText = "";
+    ele6_1.innerText = "";
+    ele7_1.innerText = "";
+    ele8_1.innerText = "";
+    ele9_1.innerText = "";
+    ele10_1.innerText = "";
+  };
+  people_6.onclick = function(){
+    people_1_maru.innerText = "";
+    people_2_maru.innerText = "";
+    people_3_maru.innerText = "";
+    people_4_maru.innerText = "";
+    people_5_maru.innerText = "";
+    people_6_maru.innerText = "○";
+    people_7_maru.innerText = "";
+    people_8_maru.innerText = "";
+    people_9_maru.innerText = "";
+    people_10_maru.innerText = "";
+    ele1_2.innerText = "×";
+    ele2_2.innerText = "×";
+    ele3_2.innerText = "×";
+    ele4_2.innerText = "×";
+    ele5_2.innerText = "×";
+    ele6_2.innerText = "×";
+    ele7_2.innerText = "";
+    ele8_2.innerText = "";
+    ele9_2.innerText = "";
+    ele10_2.innerText = "";
+    ele1_1.innerText = "";
+    ele2_1.innerText = "";
+    ele3_1.innerText = "";
+    ele4_1.innerText = "";
+    ele5_1.innerText = "";
+    ele6_1.innerText = "";
+    ele7_1.innerText = "";
+    ele8_1.innerText = "";
+    ele9_1.innerText = "";
+    ele10_1.innerText = "";
+  };
+  people_7.onclick = function(){
+    people_1_maru.innerText = "";
+    people_2_maru.innerText = "";
+    people_3_maru.innerText = "";
+    people_4_maru.innerText = "";
+    people_5_maru.innerText = "";
+    people_6_maru.innerText = "";
+    people_7_maru.innerText = "○";
+    people_8_maru.innerText = "";
+    people_9_maru.innerText = "";
+    people_10_maru.innerText = "";
+    ele1_2.innerText = "×";
+    ele2_2.innerText = "×";
+    ele3_2.innerText = "×";
+    ele4_2.innerText = "×";
+    ele5_2.innerText = "×";
+    ele6_2.innerText = "×";
+    ele7_2.innerText = "×";
+    ele8_2.innerText = "";
+    ele9_2.innerText = "";
+    ele10_2.innerText = "";
+    ele1_1.innerText = "";
+    ele2_1.innerText = "";
+    ele3_1.innerText = "";
+    ele4_1.innerText = "";
+    ele5_1.innerText = "";
+    ele6_1.innerText = "";
+    ele7_1.innerText = "";
+    ele8_1.innerText = "";
+    ele9_1.innerText = "";
+    ele10_1.innerText = "";
+  };
+  people_8.onclick = function(){
+    people_1_maru.innerText = "";
+    people_2_maru.innerText = "";
+    people_3_maru.innerText = "";
+    people_4_maru.innerText = "";
+    people_5_maru.innerText = "";
+    people_6_maru.innerText = "";
+    people_7_maru.innerText = "";
+    people_8_maru.innerText = "○";
+    people_9_maru.innerText = "";
+    people_10_maru.innerText = "";
+    ele1_2.innerText = "×";
+    ele2_2.innerText = "×";
+    ele3_2.innerText = "×";
+    ele4_2.innerText = "×";
+    ele5_2.innerText = "×";
+    ele6_2.innerText = "×";
+    ele7_2.innerText = "×";
+    ele8_2.innerText = "×";
+    ele9_2.innerText = "";
+    ele10_2.innerText = "";
+    ele1_1.innerText = "";
+    ele2_1.innerText = "";
+    ele3_1.innerText = "";
+    ele4_1.innerText = "";
+    ele5_1.innerText = "";
+    ele6_1.innerText = "";
+    ele7_1.innerText = "";
+    ele8_1.innerText = "";
+    ele9_1.innerText = "";
+    ele10_1.innerText = "";
+  };
+  people_9.onclick = function(){
+    people_1_maru.innerText = "";
+    people_2_maru.innerText = "";
+    people_3_maru.innerText = "";
+    people_4_maru.innerText = "";
+    people_5_maru.innerText = "";
+    people_6_maru.innerText = "";
+    people_7_maru.innerText = "";
+    people_8_maru.innerText = "";
+    people_9_maru.innerText = "○";
+    people_10_maru.innerText = "";
+    ele1_2.innerText = "×";
+    ele2_2.innerText = "×";
+    ele3_2.innerText = "×";
+    ele4_2.innerText = "×";
+    ele5_2.innerText = "×";
+    ele6_2.innerText = "×";
+    ele7_2.innerText = "×";
+    ele8_2.innerText = "×";
+    ele9_2.innerText = "×";
+    ele10_2.innerText = "";
+    ele1_1.innerText = "";
+    ele2_1.innerText = "";
+    ele3_1.innerText = "";
+    ele4_1.innerText = "";
+    ele5_1.innerText = "";
+    ele6_1.innerText = "";
+    ele7_1.innerText = "";
+    ele8_1.innerText = "";
+    ele9_1.innerText = "";
+    ele10_1.innerText = "";
+  };
+  people_10.onclick = function(){
+    people_1_maru.innerText = "";
+    people_2_maru.innerText = "";
+    people_3_maru.innerText = "";
+    people_4_maru.innerText = "";
+    people_5_maru.innerText = "";
+    people_6_maru.innerText = "";
+    people_7_maru.innerText = "";
+    people_8_maru.innerText = "";
+    people_9_maru.innerText = "";
+    people_10_maru.innerText = "○";
+    ele1_2.innerText = "×";
+    ele2_2.innerText = "×";
+    ele3_2.innerText = "×";
+    ele4_2.innerText = "×";
+    ele5_2.innerText = "×";
+    ele6_2.innerText = "×";
+    ele7_2.innerText = "×";
+    ele8_2.innerText = "×";
+    ele9_2.innerText = "×";
+    ele10_2.innerText = "×";
+    ele1_1.innerText = "";
+    ele2_1.innerText = "";
+    ele3_1.innerText = "";
+    ele4_1.innerText = "";
+    ele5_1.innerText = "";
+    ele6_1.innerText = "";
+    ele7_1.innerText = "";
+    ele8_1.innerText = "";
+    ele9_1.innerText = "";
+    ele10_1.innerText = "";
+  };
+    ele1_1.onclick = function(){
+      ele1_1.innerText = "";
+      ele1_2.innerText = "×";
+    };
+    ele1_2.onclick = function(){
+      ele1_2.innerText = "";
+      ele1_1.innerText = "○";
+    };
+    ele2_1.onclick = function(){
+      ele2_1.innerText = "";
+      ele2_2.innerText = "×";
+    };
+    ele2_2.onclick = function(){
+      ele2_2.innerText = "";
+      ele2_1.innerText = "○";
+    };
+    ele3_1.onclick = function(){
+      ele3_1.innerText = "";
+      ele3_2.innerText = "×";
+    };
+    ele3_2.onclick = function(){
+      ele3_2.innerText = "";
+      ele3_1.innerText = "○";
+    };
+    ele4_1.onclick = function(){
+      ele4_1.innerText = "";
+      ele4_2.innerText = "×";
+    };
+    ele4_2.onclick = function(){
+      ele4_2.innerText = "";
+      ele4_1.innerText = "○";
+    };
+    ele5_1.onclick = function(){
+      ele5_1.innerText = "";
+      ele5_2.innerText = "×";
+    };
+    ele5_2.onclick = function(){
+      ele5_2.innerText = "";
+      ele5_1.innerText = "○";
+    };
+    ele6_1.onclick = function(){
+      ele6_1.innerText = "";
+      ele6_2.innerText = "×";
+    };
+    ele6_2.onclick = function(){
+      ele6_2.innerText = "";
+      ele6_1.innerText = "○";
+    };
+    ele7_1.onclick = function(){
+      ele7_1.innerText = "";
+      ele7_2.innerText = "×";
+    };
+    ele7_2.onclick = function(){
+      ele7_2.innerText = "";
+      ele7_1.innerText = "○";
+    };
+    ele8_1.onclick = function(){
+      ele8_1.innerText = "";
+      ele8_2.innerText = "×";
+    };
+    ele8_2.onclick = function(){
+      ele8_2.innerText = "";
+      ele8_1.innerText = "○";
+    };
+    ele9_1.onclick = function(){
+      ele9_1.innerText = "";
+      ele9_2.innerText = "×";
+    };
+    ele9_2.onclick = function(){
+      ele9_2.innerText = "";
+      ele9_1.innerText = "○";
+    };
+    ele10_1.onclick = function(){
+      ele10_1.innerText = "";
+      ele10_2.innerText = "×";
+    };
+    ele10_2.onclick = function(){
+      ele10_2.innerText = "";
+      ele10_1.innerText = "○";
+    };
 });
-
 addEventListener("click",function() {
   let ele1_1 = document.getElementById("buttun1ok").innerText;
   let ele2_1 = document.getElementById("buttun2ok").innerText;
   let ele3_1 = document.getElementById("buttun3ok").innerText;
   let ele4_1 = document.getElementById("buttun4ok").innerText;
+  let ele5_1 = document.getElementById("buttun5ok").innerText;
+  let ele6_1 = document.getElementById("buttun6ok").innerText;
+  let ele7_1 = document.getElementById("buttun7ok").innerText;
+  let ele8_1 = document.getElementById("buttun8ok").innerText;
+  let ele9_1 = document.getElementById("buttun9ok").innerText;
+  let ele10_1 = document.getElementById("buttun10ok").innerText;
+  let people_1_maru_text = document.getElementById("peoplemaru_1").innerText;
+  let people_2_maru_text = document.getElementById("peoplemaru_2").innerText;
+  let people_3_maru_text = document.getElementById("peoplemaru_3").innerText;
+  let people_4_maru_text = document.getElementById("peoplemaru_4").innerText;
+  let people_5_maru_text = document.getElementById("peoplemaru_5").innerText;
+  let people_6_maru_text = document.getElementById("peoplemaru_6").innerText;
+  let people_7_maru_text = document.getElementById("peoplemaru_7").innerText;
+  let people_8_maru_text = document.getElementById("peoplemaru_8").innerText;
+  let people_9_maru_text = document.getElementById("peoplemaru_9").innerText;
+  let people_10_maru_text = document.getElementById("peoplemaru_10").innerText;
+  let check_text = document.getElementById("check-text");
   let check_form = document.getElementById("check-form");
- 
-if (ele1_1 != "" && ele1_1 == ele2_1 && ele3_1 == ele4_1 && ele2_1 == ele3_1 ){
-  // check_text.value = "2";
+  if (ele1_1 != "" &&  people_1_maru_text != ""){
+    check_text.value = "1";
+    check_form.submit();
+      alert("お疲れ様でした！\n閉店チェックOKです。\n気をつけて帰って下さい。");
+  }else if(ele1_1 != "" && ele1_1 == ele2_1 && people_2_maru_text != ""){
+  check_text.value = "2";
   check_form.submit();
     alert("お疲れ様でした！\n閉店チェックOKです。\n気をつけて帰って下さい。");
- };
+  }else if (ele1_1 != "" && ele1_1 == ele2_1 && ele2_1 == ele3_1 && people_3_maru_text != ""){
+    check_text.value = "3";
+    check_form.submit();
+      alert("お疲れ様でした！\n閉店チェックOKです。\n気をつけて帰って下さい。");
+  }else if (ele1_1 != "" && ele1_1 == ele2_1 && ele3_1 == ele4_1 && ele2_1 == ele3_1 && people_4_maru_text != ""){
+    check_text.value = "4";
+    check_form.submit();
+      alert("お疲れ様でした！\n閉店チェックOKです。\n気をつけて帰って下さい。");
+  }else if (ele1_1 != "" && ele1_1 == ele2_1 && ele3_1 == ele4_1 && ele4_1 == ele5_1 && people_5_maru_text != ""){
+    check_text.value = "5";
+    check_form.submit();
+      alert("お疲れ様でした！\n閉店チェックOKです。\n気をつけて帰って下さい。");
+  }else if (ele1_1 != "" && ele1_1 == ele2_1 && ele3_1 == ele4_1 && ele5_1 == ele6_1 && ele4_1 == ele5_1 && ele2_1 == ele3_1 && people_6_maru_text != ""){
+    check_text.value = "6";
+    check_form.submit();
+      alert("お疲れ様でした！\n閉店チェックOKです。\n気をつけて帰って下さい。");
+  }else if (ele1_1 != "" && ele1_1 == ele2_1 && ele3_1 == ele4_1 && ele5_1 == ele6_1 && ele6_1 == ele7_1 && ele2_1 == ele3_1 && people_7_maru_text != ""){
+    check_text.value = "7";
+    check_form.submit();
+      alert("お疲れ様でした！\n閉店チェックOKです。\n気をつけて帰って下さい。");
+  }else if (ele1_1 != "" && ele1_1 == ele2_1 && ele3_1 == ele4_1 && ele5_1 == ele6_1 && ele7_1 == ele8_1 && ele6_1 == ele7_1 && ele2_1 == ele3_1 && people_8_maru_text != ""){
+    check_text.value = "8";
+    check_form.submit();
+      alert("お疲れ様でした！\n閉店チェックOKです。\n気をつけて帰って下さい。");
+  }else if (ele1_1 != "" && ele1_1 == ele2_1 && ele3_1 == ele4_1 && ele5_1 == ele6_1 && ele7_1 == ele8_1 && ele8_1 == ele9_1 && ele2_1 == ele3_1 && people_9_maru_text != ""){
+    check_text.value = "9";
+    check_form.submit();
+      alert("お疲れ様でした！\n閉店チェックOKです。\n気をつけて帰って下さい。");
+  }else if (ele1_1 != "" && ele1_1 == ele2_1 && ele3_1 == ele4_1 && ele5_1 == ele6_1 && ele7_1 == ele8_1 && ele9_1 == ele10_1 && ele8_1 == ele9_1 && ele2_1 == ele3_1 && people_10_maru_text != ""){
+    check_text.value = "10";
+    check_form.submit();
+      alert("お疲れ様でした！\n閉店チェックOKです。\n気をつけて帰って下さい。");
+  }else{
+  };
 });
